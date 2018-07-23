2a2a0034e174952ae89d032598b542b8b54b8245
Philip Tenteromano <tenteromano.p@gmail.com>
2018-07-23 00:03:49 -0400

    eslint usage'

--- binary_srch.js
+++ binary_srch.js
@@ -1,29 +1,22 @@
 // a is sorted array
-let binary_Search = function(a, key, low, high) {
-  if (low < high) { return -1; }
+const binarySearch = (a, key, low, high) => {
+  if (low < high) { return -1; }   
+  const mid = (high / 2) + 1;
 
-  if (low === undefined || high === undefined) {
-    low = 0;
-    high = a.length - 1;
+  if (key === a[mid]) {
+    return mid;
   }
-  // low == mid + 1, high stays  
-  let mid = (high / 2) + 1;
-
-  if (key === a[mid]) { 
-    return mid; 
-  } else { 
-    if (key > a[mid]) {
-      return binary_search(a, key, (mid + 1), high);
-    }
-    if (key < a[mid]) {
-      high = mid - 1;
-      return binary_search(a, key, low, (mid - 1));
-    }
+  if (key > a[mid]) {
+    return binarySearch(a, key, (mid + 1), high);
   }
+  if (key < a[mid]) {
+    return binarySearch(a, key, low, (mid - 1));
+  }
+  return -1;
 };
 
-let array = [1, 50, 80, 101, 300, 2018];
-let find = 80;
+const array = [1, 50, 80, 101, 300, 2018];
+const find = 80;
 
-binary_Search(array, find);
-console.log(binary_search);
+const a = binarySearch(array, find, 0, array.length);
+console.log(a);
