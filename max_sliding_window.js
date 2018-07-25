ae7079c8fc27ad8cdcaaf03a01e133600ff559ed
Philip Tenteromano <tenteromano.p@gmail.com>
2018-07-25 16:37:20 -0400

    finishing algorithm

--- max_sliding_window.js
+++ max_sliding_window.js
@@ -7,12 +7,15 @@ let max_sliding_window = function(arr, window_size) {
   let list_ = [];
 
   // finding max of first window in array
+  // storing the INDEX of the max
   for (let i = 0; i < window_size; i++) {
-    while(list_.length > 0 && arr[i] >= arr[list_[length - 1]]) {
+    while(list_.length > 0 && arr[i] >= arr[list_[list_.length - 1]]) {
       list_.pop();
     }
     list_.push(i);
   }
+
+  return list_;
 
 
 };
