c7193b90176ff7f3ea5000245e7f787aa37dd4c2
Philip Tenteromano <tenteromano.p@gmail.com>
2018-11-15 15:16:58 -0500

    n-binocci sequence function

---
+++ nBonacci.js
@@ -0,0 +1,18 @@
+//each of its first N elements are equal to 1
+// nBon = m-n + m-(n-1) +... + m-(n==1)
+// O(n^m)
+function nBonacci(n, m) {
+  if (m <= 1 || m <= n) return 1;
+
+  let num = 0;
+  let i = n;
+  while (i >= 1) {
+    num += nBonacci(n, m - i);
+    i--;
+  }
+  return num;
+}
+
+console.log(nBonacci(3, 11)); // 193
+// adding previous 3 numbers:
+// 1,1,1,3,5,9,17,31,57,105,193
