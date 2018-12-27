76dbadd9d58f6302315686ad2585802e90b00f4f
Philip Tenteromano <tenteromano.p@gmail.com>
2018-12-27 10:57:20 -0500

    big O and valley algo

---
+++ countingValleys.js
@@ -0,0 +1,46 @@
+// Phil Tenteromano
+
+// start at sealevel
+// walking steps either UP or DOWN
+// count how many vallys are entered
+// a valley is defined as:
+// going below sealevel, and then come back to sealevel
+
+// input is a string of steps - 'UDDDUDUUU'
+
+// example of one valley:
+// _/\      _
+//    \    /
+//     \/\/
+
+// O(n) where n is the number of steps
+function countingValleys(n, s) {
+  const steps = s.split("");
+  let valleys = 0;
+  let sealevel = 0;
+  let belowSea = false;
+
+  // true if up, false if down
+  const checkUp = step => step === "U";
+
+  for (let i = 0; i < n; i++) {
+    // check relation to sealevel
+    if (checkUp(steps[i])) {
+      sealevel += 1;
+    } else sealevel -= 1;
+
+    // check if in a valley, or just came out of one
+    if (sealevel < 0) belowSea = true;
+    else if (belowSea && sealevel >= 0) {
+      valleys += 1;
+      belowSea = false;
+    }
+  }
+
+  return valleys;
+}
+
+const steps = "UDDDUUUUUDDDDDUDUUUUUU";
+const n = steps.length;
+
+console.log(countingValleys(n, steps));
---
+++ jumpingClouds.js
@@ -0,0 +1,38 @@
+// Phil Tenteromano
+
+// given a game, can only jump from one cloud to the very next (+1) or +2 away.
+// Some clouds are bad clouds, cannot jump to them
+// given a binary array of good and bad clouds (bad === 1)
+// return minimum # of jumps to win game
+// first and last cloud are always good
+
+// O(n) where n is number of clouds in the game
+function jumpingOnClouds(c) {
+  let jumps = 0;
+  const finish = c.length - 1;
+
+  const checkDone = pos => pos === finish;
+
+  let bigJ;
+  let lilJ;
+
+  for (let i = 0; i < c.length; i++) {
+    bigJ = i + 2;
+    lilJ = i + 1;
+
+    if (c[bigJ] === 0) {
+      jumps += 1;
+      if (checkDone(bigJ)) return jumps;
+      i += 1; // increment only one (for loop will increment more)
+    } else if (c[lilJ] === 0) {
+      jumps += 1;
+      if (checkDone(lilJ)) return jumps;
+    }
+  }
+}
+
+//                 1     2     3     4     5     6
+const Clouds = [0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0];
+
+console.log(jumpingOnClouds(Clouds)); // 6 jumps
+
