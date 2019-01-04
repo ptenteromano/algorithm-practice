33ec224f14479bd66fc1bf9a16b9ce6909fa464e
Philip Tenteromano <tenteromano.p@gmail.com>
2019-01-04 18:23:45 -0500

    if letters start but word not found, continue search

--- wordPuzzle.js
+++ wordPuzzle.js
@@ -4,22 +4,21 @@
 // return if the word can be found vertically
 
 const puzzle = [
-  ["A", "X", "L", "K", "G"],
-  ["B", "K", "T", "I", "Y"],
-  ["S", "I", "I", "P", "L"],
-  ["L", "N", "M", "S", "A"],
-  ["P", "B", "E", "F", "D"]
+  ["A", "X", "L", "K", "S"],
+  ["B", "K", "T", "I", "T"],
+  ["S", "I", "I", "P", "A"],
+  ["L", "N", "M", "S", "R"],
+  ["P", "B", "E", "F", "T"]
 ];
 
 function verticalCrossword(puzzle, word) {
   let index = 0;
   let checkStr = "";
   // can't start to check if the word hasn't been started and there are not enough letters left
-  let maxStartWord = puzzle.length - word.length;
-  let breaking = false;
+  let maxStartWord = puzzle.length - word.length + 1;
 
   // check every column in a row
-  for (let r = 0; r <= maxStartWord; r++) {
+  for (let r = 0; r < maxStartWord; r++) {
     for (let c = 0; c < puzzle[r].length; c++) {
       // if the first letter is found, go down that column
       if (word.charAt(index) === puzzle[r][c]) {
@@ -28,25 +27,30 @@ function verticalCrossword(puzzle, word) {
           if (word.charAt(index) === puzzle[v][c]) {
             checkStr += puzzle[v][c];
             index++;
-          } else if (checkStr === word) return true;
-          else {
-            console.log("breaking..");
-            breaking = true;
+          } else if (checkStr === word) {
+            return true;
+          } else {
+            // reset the string and try again
+            checkStr = "";
+            index = 0;
             break;
-          } //return false;
+          }
 
           // we have to check if a word ends with the end of column
-          if (checkStr === word) return true;
+          if (checkStr === word) {
+            return true;
+          }
         }
         // todo - continue checking if word isnt completed
-        if (breaking) console.log("fix this", puzzle[r][c]);
       }
     }
   }
+
   return false;
 }
 
-const words = ["ABS", "KIN", "TIME", "PINS", "notin"];
+// KIN is an example of restarting the word (it finds KIP, breaks loop and continues looking)
+const words = ["ABS", "KIN", "TIME", "START", "False"];
 
 for (let i = 0; i < words.length; i++) {
   console.log(
