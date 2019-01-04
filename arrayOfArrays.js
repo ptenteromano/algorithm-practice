b57b381a296867e5f3077d66d0315df6264dcec7
Philip Tenteromano <tenteromano.p@gmail.com>
2019-01-04 07:48:32 -0500

    array of array with concat

---
+++ arrayOfArrays.js
@@ -0,0 +1,27 @@
+// return a new 1d array with all items from 2d array
+
+function joinArrayOfArrays(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result = result.concat(arr[i]);
+  }
+
+  // return arr[0].concat(arr[1]);
+
+  return result;
+}
+
+const x = [[1, 4], [true, false], ["x", "y"]];
+
+console.log(joinArrayOfArrays(x));
+
+// bonus: use of reduce
+
+function sum(a, b) {
+  return a + b;
+}
+
+const y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(y.reduce(sum, 0));
