ebf2cf7cfc984ed6ba2c4ba91eaf537d352506a6
Philip Tenteromano <tenteromano.p@gmail.com>
2019-05-31 23:17:15 -0400

    matching brackets algo

---
+++ matching-brackets.js
@@ -0,0 +1,20 @@
+// Using stack + dictionary
+export const matchingBrackets = str => {
+  const stack = [];
+  const nests = {
+    "(": ")",
+    "{": "}",
+    "[": "]"
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+
+    if (char === "(" || char === "{" || char === "[") stack.push(char);
+    else if (char === ")" || char === "}" || char === "]") {
+      let frontBracket = stack.pop();
+      if (nests[frontBracket] !== char) return false;
+    }
+  }
+  return stack.length === 0;
+};
