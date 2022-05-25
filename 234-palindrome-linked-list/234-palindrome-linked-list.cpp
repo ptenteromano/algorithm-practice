f3bae23764a36df941bc6eb541dd6d6755de1701
Phil Tenteromano <philip.tenteromano@gmail.com>
2022-05-25 17:54:31 -0400

    Time: 224 ms (90.68%), Space: 114.1 MB (85.14%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,19 +11,35 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        std::vector< int > arr;
+        ListNode *slow = head, *fast = head;
+        ListNode *prev, *temp;
         
-        while (head != nullptr) {
-            arr.push_back(head->val);
-            head = head->next;
+        // Floyds algo to find end and middle
+        while (fast && fast->next) 
+            slow = slow->next, fast = fast->next->next;
+        
+        // Start the reversal
+        prev = slow;
+        slow = slow->next;
+        prev->next = nullptr;
+        
+        // Reverse everything from slow till the end
+        while (slow) {
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
         }
-        int leftIdx = 0, rightIdx = arr.size() - 1;
         
-        while (leftIdx < rightIdx) {
-            if (arr[leftIdx] != arr[rightIdx])
-                return false;
-            leftIdx++;
-            rightIdx--;
+        // Prev now always points to the last node in the list
+        fast = head;
+        slow = prev;
+        
+        // Now Compare
+        while (slow) {
+            if (fast->val != slow->val) return false;
+            slow = slow->next;
+            fast = fast->next;
         }
         
         return true;
