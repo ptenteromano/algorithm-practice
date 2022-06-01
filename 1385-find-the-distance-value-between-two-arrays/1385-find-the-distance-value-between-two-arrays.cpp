c2fe76a5c3c903b6763e3ff3dfcd403694f33d33
Phil Tenteromano <philip.tenteromano@gmail.com>
2022-06-01 12:41:32 -0400

    Time: 17 ms (47.60%), Space: 13.1 MB (29.79%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        int count = arr1.size(), absValue;
-        cout << d << '\n';
+        int count = arr1.size(), absValue, j;
+        sort(arr2.begin(), arr2.end());
+
         for (int i = 0; i < arr1.size(); i++) {
-            for (int j = 0; j < arr2.size(); j++) {
-                absValue = abs(arr1[i] - arr2[j]);
-                cout << "abs: " << absValue << '\n';
-                
-                if (absValue <= d) {
-                    count--;
-                    break;
-                }
-                
+            j = closestIdx(arr2, arr1[i]);
+            absValue = abs(arr1[i] - arr2[j]);
+            
+            if (absValue <= d) 
+                count--;
+        }
+        return count;
+    }
+
+private:
+    // Binary Search for closest value to target, return idx;
+    int closestIdx(vector<int>&arr2, int target) {
+        int low = 0, high = arr2.size() - 1, mid;
+        int followClose = INT_MAX, absValue, closest;
+        
+        while (low <= high) {
+            mid = ceil((high + low) / 2.0);
+            
+            if (arr2[mid] == target) return mid;
+            
+            absValue = abs(arr2[mid] - target);
+            if (absValue < followClose) {
+                followClose = absValue;
+                closest = mid;
             }
             
-            cout << "------\n";
+            if (arr2[mid] < target) 
+                low = mid + 1;
+            else 
+                high = mid - 1;
         }
         
-        cout << "DONE\n";
-        return count;
+        return closest;
     }
 };
