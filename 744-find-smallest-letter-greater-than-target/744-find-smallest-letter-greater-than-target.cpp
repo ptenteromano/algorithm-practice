b55390e1159810ed062f5c7136fada942f29dec2
Phil Tenteromano <philip.tenteromano@gmail.com>
2022-06-01 13:48:48 -0400

    Time: 27 ms (27.17%), Space: 16 MB (26.44%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -7,13 +7,12 @@ public:
         while (low <= high) {
             mid = ceil((high + low) / 2.0);
             
-            if (letters[mid] <= target) 
-                low = mid + 1;
-            else if (letters[mid] > target) 
+            if (letters[mid] > target) 
                 high = mid - 1;
+            else
+                low = mid + 1;
         }
         
-        const int idx = max(low, high) > len ? 0 : max(low, high);
-        return letters[idx];
+        return letters[low % (len + 1)];
     }
 };
