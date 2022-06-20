9f0a11fea0b03be7a0294f020880d399ab2ad31e
Phil Tenteromano <philip.tenteromano@gmail.com>
2022-06-19 21:36:49 -0400

    Time: 76 ms (6.74%), Space: 6.9 MB (61.00%) - LeetHub

---
+++ 704-binary-search/704-binary-search.go
@@ -0,0 +1,19 @@
+func search(nums []int, target int) int {
+    low, high := 0, len(nums) - 1
+    
+    for low <= high {
+        mid := (high + low) / 2
+        
+        if nums[mid] == target {
+            return mid
+        }
+        
+        if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    
+    return -1
+}
