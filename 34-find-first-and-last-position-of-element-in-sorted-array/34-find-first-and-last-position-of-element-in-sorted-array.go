b9ab44b3abe6ce416fb1f993512a16fd6a31331a
Phil Tenteromano <philip.tenteromano@gmail.com>
2022-06-24 12:45:33 -0400

    Time: 14 ms (20.51%), Space: 4.1 MB (13.64%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.go
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.go
@@ -2,36 +2,52 @@ func searchRange(nums []int, target int) []int {
     // Initial Search
     high := findTarget(nums, target)
     
+    // Didn't find it, return early
     if high == -1 {
         return []int{-1, -1}
     }
     
-    // We found it, lets search left and right
-    low := high
+    // Split the array and parse both sides
+    // This is a good problem for concurrency, as each side is completely separate memory addresses
+    bounds := make(chan int, 2)
     
-    // Search lower half of array
-    for {
-        temp := findTarget(nums[:low], target)
-        if temp == -1 {
-            break
+    // Search lower half of array, in its own thread (goroutine)
+    go func(low int) {
+        for {
+            temp := findTarget(nums[:low], target)
+            if temp == -1 {
+                break
+            }
+            low = temp
+        } 
+        bounds<-low
+    }(high)
+    
+    
+    // Search upper half of array, in its own thread (goroutine)
+    go func(high int) {
+        for {
+            if len(nums[high + 1:]) == 0 {
+                break
+            }
+
+            temp := findTarget(nums[high + 1:], target)
+            if temp == -1 {
+                break
+            }
+            high += temp + 1
         }
-        low = temp
+        bounds<-high
+    }(high)
+    
+    newLow := <-bounds
+    newHigh := <-bounds
+    
+    if newLow > newHigh {
+        newLow, newHigh = newHigh, newLow
     }
     
-    // Search upper half of array
-    for {
-        if len(nums[high + 1:]) == 0 {
-            break
-        }
-        
-        temp := findTarget(nums[high + 1:], target)
-        if temp == -1 {
-            break
-        }
-        high += temp + 1
-    }
-    
-    return []int{low, high}
+    return []int{newLow, newHigh}
 }
 
 // Pass a slice in, binary search
